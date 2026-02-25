3af59c51d7d88683d16fdf6e3235ceb33d80119d
Kuruva Pavani <pavanikuruva2109@gmail.com>
2026-02-25 06:29:45 +0530

    setup basic express server running on port 3000

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Server is running" });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
